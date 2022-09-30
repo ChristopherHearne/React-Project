a547eded7bc44770c4e1cee9fa53d576904fe4a6
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-09-30 12:49:01 +0200

    created new navbar component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import About from './components/About'
 import Footer from './components/Footer'
 import Info from './components/Info'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Info />
       <About />
       <Footer />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Navbar(){
+    return <h1>This is going to be the navbar</h1>
+}
