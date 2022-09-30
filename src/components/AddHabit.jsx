5f295fa1d161400e94e38166b618d313e0fb5a03
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-09-30 14:36:20 +0200

    input now registers change and logs it

--- src/components/AddHabit.jsx
+++ src/components/AddHabit.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import {useState, useEffect} from 'react'
 
 export default function AddHabit(){
+    const [habit, setHabit] = useState('')
+    function addHabit(){
+
+    }
+
+    const handleChange = event => {
+        setHabit(event.target.value)
+    }
     return(
         <div className="add--habit--container">
             <div className="input--container">
-                <input type="text" placeholder="Add a habit..."></input>
-                <button>Add</button> 
+                <input 
+                    className="input--text"
+                    type="text" 
+                    placeholder="Add a habit..."
+                    value={habit}
+                    onChange={handleChange}
+                />
+                <button className="add--btn" onClick={addHabit}>Add</button> 
             </div>
             <div className="habits--container">
                 <div className="habit--item">
