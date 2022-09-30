6910b597925d5d180e9f936cdfc6c39390394beb
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-09-30 13:07:42 +0200

    added squeed logo and minor styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-
+import Logo from '../images/logo-squeed-v3.png'
 export default function Navbar(){
-    return <h1>This is going to be the navbar</h1>
+    return (
+        <nav className="navbar--container">
+            <img className="navbar--logo" src={Logo} alt="Squeed Logo"></img>
+            <a>Home</a>
+            <a>Scorecard</a>
+            <a>Habit stacking</a>
+            <a>Stats</a>
+        </nav> 
+    )
 }
