147adba09600d13d0f0c2e09117d4bed74e17644
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-03 14:21:38 +0200

    mockup of categorycomponent completed, styled text-input

--- src/components/CategoryComponent.jsx
+++ src/components/CategoryComponent.jsx
@@ -4,13 +4,40 @@ export default function CategoryComponent(){
     return(
         <div className="category--container">
             <div className="positive--container">
-                <h3>Positive</h3>
+                <div className="category--header">
+                    <h3>Positive</h3>
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+                <div className="category--list">
+                    <div className="category--item--positive">
+                        <span className="category--item--name">Smoking</span>
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                </div>
             </div>
             <div className="neutral--container">
-                <h3>Neutral</h3>
+                <div className="category--header">
+                    <h3>Neutral</h3>
+                    <i class="fa-solid fa-equals"></i>
+                </div>
+                <div className="category--list">
+                    <div className="category--item--neutral">
+                        <span className="category--item--name">Smoking</span>
+                        <i class="fa-solid fa-equals"></i>
+                    </div>
+                </div>
             </div>
             <div className="negative--container">
-                <h3>Negative</h3>
+                <div className="category--header">
+                    <h3>Negative</h3>
+                    <i class="fa-solid fa-minus"></i>
+                </div>
+                <div className="category--list">
+                    <div className="category--item--negative">
+                        <span className="category--item--name">Smoking</span>
+                        <i class="fa-solid fa-minus"></i>
+                    </div>
+                </div>
             </div>
         </div>
     )
