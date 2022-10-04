7994958a4071f4242051aaf91db26c5aeb5f31e8
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-04 13:23:10 +0200

    google sign in button implemented with new client package

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,27 +3,31 @@ import {useState, useEffect, useRef} from 'react'
 export default function SignIn(){
     const googleSignInButton = useRef(null)
     
-    const onResponse = async ({credential}) => {
-        console.log(credential)
+    const onResponse = async (response) => {
+        // const responsePayload = decodeJwtResponse(response.credential)
+        console.log(response.payload)
     }
-    window.google.accounts.id.initialize({
-        client_id: "896013867387-ltnfg30uau25jqguvdd35v1ubp35u58l.apps.googleusercontent.com",
-        callback: onResponse,
-        auto_select: false
-    })
     
-    window.google.accounts.id.renderButton(
-        googleSignInButton.current, 
-        { theme: "outline", size: "large", text:'signin_with', width: "250" }
-    )
+    useEffect(() => {
+        if (googleSignInButton.current) {
+          window.google.accounts.id.initialize({
+            client_id: '896013867387-ltnfg30uau25jqguvdd35v1ubp35u58l.apps.googleusercontent.com',
+            callback: (res, error) => {
+              console.log(res)
+            },
+          });
+          window.google.accounts.id.renderButton(googleSignInButton.current, {
+            theme: 'outline',
+            size: 'large',
+            type: 'standard',
+            text: 'signin_with',
+          });
+        }
+      }, [googleSignInButton.current]);
         
     const onClick = () => {
         window.google.accounts.id.prompt()
     }
 
-    return (
-        <div className="testing">
-            <div className="test" ref={googleSignInButton} onClick={onClick}></div>
-        </div>
-    )
+    return <div className="test" ref={googleSignInButton} onClick={onClick}></div>
 }
