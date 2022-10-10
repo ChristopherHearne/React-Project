b18b9a78f1396ea8887fdbebbdc71ad52a4ce798
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-10 14:39:42 +0200

    added page information to each page to be able to identify them

--- src/pages/HabitStacking.jsx
+++ src/pages/HabitStacking.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function HabitStacking(){
     return(
-        <h1>This is the home page</h1>
+        <h1>This is the Habit Stacking page</h1>
     )
 }
--- src/pages/Manifesto.jsx
+++ src/pages/Manifesto.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Manifesto(){
     return(
-        <h1>This is the home page</h1>
+        <h1>This is the Manifesto page</h1>
     )
 }
